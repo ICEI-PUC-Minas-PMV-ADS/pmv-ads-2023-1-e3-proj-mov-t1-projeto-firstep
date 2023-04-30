8dc110083095c01edbbe96119805f6dfb048cf5b
Jéssica Grimaldi <jessicagrimaldisa@gmail.com>
2023-04-29 22:04:14 -0300

    Insere import Cadastro do Usuário

--- src/Mobile/app-Firstep/App.js
+++ src/Mobile/app-Firstep/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import CadastroProjeto from './pages/CadastroProjeto';
 import Login from './pages/Login';
 import Inscrever from './pages/Inscrever';
+import CadastroUsuario from './pages/CadastroUsuario';
 
 
 
