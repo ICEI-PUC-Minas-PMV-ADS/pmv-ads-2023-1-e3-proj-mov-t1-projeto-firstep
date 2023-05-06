95933d4eb25a302bc73f539746813ccb5e225525
Karen Noguti <jkjvida@hotmail.com>
2023-05-06 20:04:55 +0900

    Update Banco_de_dados.sql
    Inclusão da tabela UsuarioProjeto

--- src/Banco_de_dados.sql
+++ src/Banco_de_dados.sql
@@ -4,7 +4,6 @@ USE Firstep;
 
 -- Criando as Tabelas do Banco de Dados--
 
-
 create table Usuario (Id char(5) Primary Key,
 Nome char(30) NOT NULL, 
 Email char(20) NOT NULL, 
@@ -13,7 +12,6 @@ DescricaoPerfil char(255) NOT NULL,
 Github char(255)NOT NULL);
 
 create table Projeto  (Id char(5) Primary Key, 
-IdUsuario char (5), 
 NomeProjeto char(30) NOT NULL,
 DescricaoProjeto char (255) NOT NULL,
 Tecnologias  char(255) NOT NULL, 
@@ -21,18 +19,16 @@ DescricaoVaga char(255) NOT NULL,
 Repositorio char(50) NOT NULL,
 AutorProjeto char(30) NOT NULL,
 QuantParticipante int NOT NULL,
-Constraint FK_Projeto_Usuario foreign Key (IdUsuario) References Usuario(Id));
+Finalizado  boolean NOT NULL,
+ParticipantesProjeto char(255) NOT NULL);
 
-select * from Usuario;
-select * from projeto;
+create table UsuarioProjeto (Id char(5) Primary Key, 
+IdUsuario char (5), 
+IdProjeto char (5), 
+Constraint FK_UsuarioProjeto_Usuario foreign Key (IdUsuario) References Usuario(Id),
+Constraint FK_UsuarioProjeto_Projeto foreign Key (IdProjeto) References Projeto(Id),
+Constraint UQ_IdUsuario_IdProjeto UNIQUE (IdUsuario,IdProjeto));
 
-iNSERT INTO Usuario values ('001', 'Hannah Montana', 'hannah@montana.com','12345','Estou estudando back-end e gostaria de participar de projetos para desenvolver essa habilidade','github.com/hannah');
-
-iNSERT INTO Projeto values ('001', '001', 'Agricont', 'O projeto consiste em uma página web para gestão patrimonial de agricultores.','HTML, CSS, BootStrap
-.NET C#
-SQLServer',' Dev fullstack jr
-Empresa: Juninhos
-Salário: À combinar',' github.com/AgriCont', 'junior123@gmail.com',3);
 
 
 
