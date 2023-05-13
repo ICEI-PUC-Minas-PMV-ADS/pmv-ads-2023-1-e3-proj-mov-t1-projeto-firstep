1658f90d4e9bff0ec4c664f7252acbb0a6a271d9
ClaudiaCarapiaFerraz <102997121+ClaudiaCarapiaFerraz@users.noreply.github.com>
2023-05-13 14:35:10 -0300

    Update Projetos.services.js

--- src/Mobile/app-Firstep/services/Projetos.services.js
+++ src/Mobile/app-Firstep/services/Projetos.services.js
@@ -13,3 +13,53 @@ export async function getProjetos(id){
     }
 }
 
+export const insertProjetos = async (param) => {
+    try{
+      return await API.post(`${BASE_URL}/projetos`, param).then( 
+        response => {
+          return response.data;
+        },
+        error =>{
+          console.log(error);
+          return  null;
+        }
+      );
+    }catch(error){
+      console.log(error);
+      return null;
+    }
+  }
+
+  export const updateProjetos = async (param) => {
+    try{
+      return await API.put(`${BASE_URL}/projetos/${param.id}`, param).then( 
+        response => {
+          return response.data;
+        },
+        error =>{
+          console.log(error);
+          return  null;
+        }
+      );
+    }catch(error){
+      console.log(error);
+      return null;
+    }
+  }
+
+  export const deleteProjetos = async (id) => {
+    try{
+      return await API.delete(`${BASE_URL}/projetos/${id}`, param).then( 
+        response => {
+          return response.data;
+        },
+        error =>{
+          console.log(error);
+          return  null;
+        }
+      );
+    }catch(error){
+      console.log(error);
+      return null;
+    }
+  }
