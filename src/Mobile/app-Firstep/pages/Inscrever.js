348e86fcea15ad7f6660612f82ef4b482ff299a2
Karen Noguti <jkjvida@hotmail.com>
2023-05-13 09:41:12 +0900

    Update Inscrever.js

--- src/Mobile/app-Firstep/pages/Inscrever.js
+++ src/Mobile/app-Firstep/pages/Inscrever.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import {View,StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { useIsFocused } from '@react-navigation/native';
+import { getProjetos } from '../services/Inscrever.services';
 
 import Container from '../components/Container';
 import Logo from '../components/Logo';
 import Card from '../components/Card';
 import Body from '../components/Body';
-import { getProjetos } from '../services/Inscrever.services';
-
 
 const Inscrever = () => {
   const [id, setId] = useState('');
@@ -20,26 +19,13 @@ const Inscrever = () => {
   const [autorProjeto, setAutorProjeto] = useState('');
   const [quantidadeParticipante, setquantidadeParticipante] = useState('');
 
-  const DATA = {
-    id: 1,
-    nomeProjeto: 'Agricont',
-    descricaoProjeto:
-      'O projeto consiste em uma página web para gestão patrimonial de agricultores.',
-    tecnologias: 'HTML, CSS, BootStrap, NET C#,SQLServer',
-    descricaoVaga: 'Empresa: Juninho - Dev fullstack jr -  Salário: À Combinar',
-    repositorio:
-      'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-2-e2-proj-int-t5-projeto-agricont',
-    autorProjeto: 'Maria José',
-    quantidadeParticipante: 3,
-  };
-
   const isFocused = useIsFocused();
 
-  useEffect (() => {
-fetchProjetos()
+  useEffect(() => {
+    fetchProjetos()
   }, [isFocused])
 
-  async function fetchProjetos(){
+  async function fetchProjetos() {
 
     const res = await getProjetos("1")
     setId(res.id)
@@ -49,9 +35,8 @@ fetchProjetos()
     setTecnologias(res.tecnologias)
     setdescricaoVaga(res.descricaoVaga)
     setquantidadeParticipante(res.quantidadeParticipante)
-  
-  }
 
+  }
   return (
     <Container>
       <ScrollView>
@@ -59,37 +44,37 @@ fetchProjetos()
           <Logo />
         </View>
         <Body>
-        <Text style={styles.titulo}>{nomeProjeto}</Text>
-        <Card>
-          <Text style={styles.texto}>{descricaoProjeto}</Text>
-        </Card>
-        <Card>
-          <Text style={styles.texto}>{descricaoVaga}</Text>
-        </Card>
-        <Card>
-          <Text style={styles.texto}>Tecnologias Utilizadas: </Text>
-          <Text style={styles.texto}>{tecnologias}</Text>
-        </Card>
+          <Text style={styles.titulo}>{nomeProjeto}</Text>
+          <Card>
+            <Text style={styles.texto}>{descricaoProjeto}</Text>
+          </Card>
+          <Card>
+            <Text style={styles.texto}>{descricaoVaga}</Text>
+          </Card>
+          <Card>
+            <Text style={styles.texto}>Tecnologias Utilizadas: </Text>
+            <Text style={styles.texto}>{tecnologias}</Text>
+          </Card>
 
-        <Text style={styles.quantPart}>
-          Quantidade de Participantes: {quantidadeParticipante}
-        </Text>
+          <Text style={styles.quantPart}>
+            Quantidade de Participantes: {quantidadeParticipante}
+          </Text>
 
-        <Button
-          style={styles.button}
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Candidatar-se
-        </Button>
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={() => console.log('Pressed')}>
+            Candidatar-se
+          </Button>
 
-        <Button
-          style={styles.button}
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Voltar
-        </Button>
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={() => console.log('Pressed')}>
+            Voltar
+          </Button>
         </Body>
-        </ScrollView>
+      </ScrollView>
     </Container>
   );
 };
@@ -118,10 +103,11 @@ const styles = StyleSheet.create({
     marginHorizontal: 40,
     textAlign: 'center',
   },
-  
+
   logo: {
     alignItems: 'center',
     marginTop: 10,
   },
 });
+
 export default Inscrever;
