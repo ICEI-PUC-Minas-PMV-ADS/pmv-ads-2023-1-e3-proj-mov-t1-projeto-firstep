f0108a4ea7c00a154b67fe976cca350ea3071830
keirosu <103314640+keirosu@users.noreply.github.com>
2023-06-03 20:41:45 +0900

    Update Login.js

--- src/Mobile/app-Firstep/pages/Login.js
+++ src/Mobile/app-Firstep/pages/Login.js
@@ -1,52 +1,41 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
-import { TextInput, Button, Headline } from 'react-native-paper';
-import Container from '../components/Container';
-import Body from '../components/Body';
-import Input from '../components/Input';
-import Logo from '../components/Logo';
+import React, { useState } from "react";
+import { StyleSheet, View, Alert,ScrollView } from "react-native";
+import { TextInput, Button, Headline } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { useUser } from "../contexts/UserContext";
+import { login } from "../services/Auth.services";
 
-import { useNavigation } from '@react-navigation/native';
-import {useUser} from '../contexts/UserContext';
-
-import {login} from '../services/Auth.services';
+import Container from "../components/Container";
+import Body from "../components/Body";
+import Input from "../components/Input";
+import Logo from "../components/Logo";
 
 const Login = () => {
-
   const navigation = useNavigation();
-
   const { setSigned } = useUser();
+  const [email, setEmail] = useState("firstep@gmail.com");
+  const [password, setPassword] = useState("firstep");
 
-  const [email, setEmail] = useState('firstep@gmail.com');
-  const [password, setPassword] = useState('firstep');
-
-    const handleLogin= () => {
-
+  const handleLogin = () => {
     login({
       email: email,
-      password: password
-    }).then( res => {
+      password: password,
+    }).then((res) => {
       console.log(res);
 
-      if(res && res.user){
+      if (res && res.user) {
         setSigned(true);
         setName(res.user.name);
-        AsyncStorage.setItem('@TOKEN_KEY', res.accessToken).then();
-      }else{
-         Alert.alert('Atenção', 'Usuário ou senha inválidos!');
+        AsyncStorage.setItem("@TOKEN_KEY", res.accessToken).then();
+      } else {
+        Alert.alert("Atenção", "Usuário ou senha inválidos!");
       }
-
     });
-    
-  }
-
+  };
   return (
-    <Container>
-      <View style={styles.header}>
-        <Logo />
-      </View>
-
-      
+    <Container> 
+       <ScrollView>    
+        <Logo />    
 
       <Body>
         <Input
@@ -65,16 +54,17 @@ const Login = () => {
         <Button
           style={styles.button}
           mode="contained"
-          onPress={() => setSigned(true)}>
+          onPress={() => setSigned(true)}>        
           LOGIN
         </Button>
         <Button
           style={styles.button}
           mode="contained"
-          onPress={() => navigation.navigate('Register')}>
+          onPress={() => navigation.navigate("Register")}>       
           Registrar
         </Button>
       </Body>
+      </ScrollView> 
     </Container>
   );
 };
@@ -83,14 +73,9 @@ const styles = StyleSheet.create({
   button: {
     marginVertical: 8,
     marginHorizontal: 40,
-    backgroundColor: '#3E2500',
+    backgroundColor: "#3E2500",
   },
  
-  header: {
-    alignItems: 'center',
-    marginTop: 30,
-    marginBottom: 12
-  },
 });
 
 export default Login;
